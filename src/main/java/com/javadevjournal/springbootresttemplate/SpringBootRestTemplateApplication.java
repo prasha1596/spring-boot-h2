17dd0b1f5e9dc945aa1bdfa362a1b5db7bd00ec3
prash <prahsa1596@gmail.com>
2022-02-09 17:45:35 +0530

    Added timeout

--- src/main/java/com/javadevjournal/springbootresttemplate/SpringBootRestTemplateApplication.java
+++ src/main/java/com/javadevjournal/springbootresttemplate/SpringBootRestTemplateApplication.java
@@ -3,6 +3,8 @@ package com.javadevjournal.springbootresttemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -15,6 +17,19 @@ public class SpringBootRestTemplateApplication {
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean("restTemplateWithTimeout")
+    RestTemplate restTemplateWithTimeout() {
+        return new RestTemplate(getClientHttpRequestFactory());
+    }
+
+    private ClientHttpRequestFactory getClientHttpRequestFactory() {
+        int timeout = 5000;
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(timeout);
+        return clientHttpRequestFactory;
     }
 
 }
